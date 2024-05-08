682f220c8197951a44ef85822dcb615b65aeb1a6
emikaw <emilkakawka111@gmail.com>
2024-05-08 12:58:00 +0200

    Code correction + plots

--- SVM.py
+++ SVM.py
@@ -1,30 +1,33 @@
-# ================================== BIBLIOTEKI ==================================
+# ================================== LIBRARIES ==================================
 
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit, KFold
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.linear_model import LinearRegression
-from sklearn import datasets, svm, metrics, linear_model
-import warnings
+from sklearn import datasets, svm, metrics
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-# ================================== PROBLEMY REGRESYJNE  ==================================
-# ZAŁADOWANIE PRZYKŁADOWYCH DANYCH dla regresji z 'sklearn datasets'
-X, y = datasets.load_iris(return_X_y=True)
-print(X.shape, y.shape)
+# ================================== REGRESSION  ==================================
+print("-"*30,"REGRESSION","-"*30)
+iris = datasets.load_iris()
+X = iris.data[:, 2].reshape(-1, 1)
+y = iris.data[:, 3]                
 
-# STWORZENIE MODELU
+# MODEL
+print("[INFO] Creation a model...")
 modelRegress = LinearRegression()
 
-# PODZIAŁ DANYCH
-# Ustalona z góry proporcja -------------------------------------------------------
+# DATA SPLIT
+# Fixed proportion (here: 80%-20%)
+print("[INFO] Data splitting...")
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=2)
 modelRegress.fit(X_train, y_train)
 train_score = modelRegress.score(X_train, y_train)
 test_score = modelRegress.score(X_test, y_test)
-print(f"Train_score = {train_score}\nTest score = {test_score}")
+print(f"[INFO] Calculation scores for fixed ratio...\n\nTrain_score = {train_score}\nTest score = {test_score}")
 
-# Walidacja krzyżowa --------------------------------------------------------------
+# Cross-validation
+print("\n[INFO] Cross-validation initiation...")
 kfold = KFold(n_splits=6, random_state=42, shuffle=True)
 
 train_scores = []
@@ -36,47 +39,71 @@ for (train_index, test_index) in kfold.split(X, y):
     y_train, y_test = y[train_index], y[test_index]
     modelRegress.fit(X_train, y_train)
 
-    # Ocena zbioru treningowego
+    # Score for training set
     train_score2 = modelRegress.score(X_train, y_train)
     train_scores.append(train_score2)
 
-    # Ocena na zbiorze testowym
+    # Score for test set
     test_score2 = modelRegress.score(X_test, y_test)
     test_scores.append(test_score2)
 
-# Wszystkie wyniki CV
-print(f"\nTrain scores = {train_scores}\nTest scores = {test_scores}")
+# All CV scores
+print(f"[INFO] Calculation scores for cross-validation...\n\nTrain scores = {train_scores}\nTest scores = {test_scores}\n")
 
-# Średni wynik CV
+# Mean CV scores
+print(f"[INFO] Calculation mean scores for cross-validation...")
 avg_train_score = np.mean(train_scores)
 avg_test_score = np.mean(test_scores)
 
 print(f"\nAverage train score = {avg_train_score}\nAverage test score = {avg_test_score}")
 
-# EWALUACJA MODELU (MSE, RMSE)
-
+# MODEL EVALUATION (MSE, RMSE)
+print("\n[INFO] Model evaluation initiation...")
 y_pred = modelRegress.predict(X_test)
 mse = metrics.mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
-print(f"\nMSE = {mse}\nRMSE = {rmse}")
+print(f"[INFO] Evaluating...\n\nMSE = {mse}\nRMSE = {rmse}")
 
-# ================================== PROBLEMY KLASYFIKACYJNE  ==================================
-# ZAŁADOWANIE PRZYKŁADOWYCH DANYCH dla klasyfikacji z 'sklearn datasets'
-X, y = datasets.load_wine(return_X_y=True)
-print(X.shape, y.shape)
+# Actual vs predicted plot
+print("\n[INFO] Generating 'Actual vs predicted plot'...")
+plt.scatter(y_test, y_pred)
+plt.title('Actual vs Predicted Model')
+plt.xlabel('Actual values')
+plt.ylabel('Predicted values')
+m, b = np.polyfit(y_test, y_pred, 1)
+plt.plot(y_test, m*y_test+b, color = 'r')
+plt.show()
 
-# STWORZENIE MODELU
+
+# Residual plot
+print("[INFO] Generating 'Residual plot'...\n")
+residuals = y_test - y_pred
+plt.scatter(y_pred, residuals)
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.title('Residual Plot')
+plt.axhline(y = 0, color = 'r', linestyle = '--')
+plt.show()
+
+# ================================== CLASSIFICATION PROBLEMS  ==================================
+print("-"*30,"CLASSIFICATION","-"*30)
+X, y = datasets.load_breast_cancer(return_X_y=True)
+
+# MODEL
+print("[INFO] Creation a model...")
 modelClass = svm.SVC(kernel='linear', C=1)
 
-# PODZIAŁ DANYCH
-# Z góry ustalony podział
+# DATA SPLIT
+# Fixed ratio
+print("[INFO] Data splitting...")
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=2)
 modelClass.fit(X_train, y_train)
 train_score = modelClass.score(X_train, y_train)
 test_score = modelClass.score(X_test, y_test)
-print(f"Train_score = {train_score}\nTest score = {test_score}")
+print(f"\n[INFO] Calculation scores for fixed ratio...\n\nTrain_score = {train_score}\nTest score = {test_score}")
 
-# Walidacja krzyżowa
+# Cross-validation
+print("\n[INFO] Cross-validation initiation...")
 kfold = KFold(n_splits=6, random_state=42, shuffle=True)
 
 train_scores = []
@@ -85,29 +112,32 @@ test_scores = []
 for (train_index, test_index) in kfold.split(X, y):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
-    modelRegress.fit(X_train, y_train)
+    modelClass.fit(X_train, y_train)
 
-    # Ocena zbioru treningowego
-    train_score2 = modelRegress.score(X_train, y_train)
+    # Score for training set
+    train_score2 = modelClass.score(X_train, y_train)
     train_scores.append(train_score2)
 
-    # Ocena na zbiorze testowym
-    test_score2 = modelRegress.score(X_test, y_test)
+    # Score for test set
+    test_score2 = modelClass.score(X_test, y_test)
     test_scores.append(test_score2)
 
-# Wszystkie wyniki CV
-print(f"\nTrain scores = {train_scores}\nTest scores = {test_scores}")
+# All CV scores
+print(f"[INFO] Calculation scores for cross-validation...\n\nTrain scores = {train_scores}\nTest scores = {test_scores}")
 
-# Średni wynik CV
+# Mean CV scores
+print(f"\n[INFO] Calculation mean scores for cross-validation...")
 avg_train_score = np.mean(train_scores)
 avg_test_score = np.mean(test_scores)
 
 print(f"\nAverage train score = {avg_train_score}\nAverage test score = {avg_test_score}")
 
-# EWALUACJA MODELU
+# MODEL EVALUATION
+print("\n[INFO] Model evaluation initiation...")
 y_pred = modelClass.predict(X_test)
 
-# Obliczenie TP, TN, FP i FN
+# tp, tn, fp, fn calculation
+print("[INFO] Calculation of tp, tn, fp and fn...")
 tp = 0
 tn = 0
 fp = 0
@@ -123,11 +153,21 @@ for y_value in range(len(y_pred)):
     elif y_pred[y_value] == 0 and y_test[y_value] == 0:
         tn += 1
 
-# Metryki
-accuracy = (tp + tn) / (tp + tn + fp + fn)
-precision = tp / (tp + fp)
-recall = tp / (tp + fn)
-fscore = (2 * precision * recall) / (precision + recall)
+# Confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+display = ConfusionMatrixDisplay(confusion_matrix=cm)
+display.plot(cmap="Blues")
+plt.title("Confusion matrix")
+plt.show()
 
-print(f"\nAccuracy = {accuracy}\nPrecision = {precision}\nRecall = {recall}\nF-score = {fscore}")
+# Metrics
+print("[INFO] Calculation of metrics...")
+try:
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    fscore = (2 * precision * recall) / (precision + recall)
+    print(f"\nAccuracy = {accuracy}\nPrecision = {precision}\nRecall = {recall}\nF-score = {fscore}")
+except ZeroDivisionError:
+    print("[ERROR] Division by zero is not possible!")
 
